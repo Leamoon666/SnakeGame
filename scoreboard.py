e0507762023994a35866f429e55be1410a4d76b1
Leamoon666 <leamoon666.po4ta@gmail.com>
2025-08-01 17:16:57 +0300

    add high score file

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.pencolor("white")
         self.penup()
@@ -18,9 +20,14 @@ class Scoreboard(Turtle):
         self.score += 1
         self.current_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as high:
+                high.write(str(self.high_score))
+        self.score = 0
+        self.current_score()
 
     def current_score(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font=FONT)
